81efd038128b5b9a438fdd6d9d09ed5029d72373
PHATCHARAPHON63 <phatcharaphonch63@nu.ac.th>
2024-03-09 12:46:09 +0700

    v2

--- src/calculator/lexer.py
+++ src/calculator/lexer.py
@@ -1,101 +1,55 @@
-from enum import Enum, auto
-
-# Define TokenType and Token
-class TokenType(Enum):
-    NUMBER = auto()
-    ADD_OP = "+"
-    SUB_OP = "-"
-    MUL_OP = "*"
-    DIV_OP = "/"
-    POWER_OP = "^"
-    FAC_OP = "!"
-    LEFT_PAREN = "("
-    RIGHT_PAREN = ")"
-    EOF = auto()
-
-class Token:
-    def __init__(self, type, value=None):
-        self.type = type
-        self.value = value
-
-    def __repr__(self):
-        return f"Token({self.type}, {repr(self.value)})"
-
-# Lexer class
 class Lexer:
     def __init__(self, text):
         self.text = text
         self.pos = 0
-        self.current_char = self.text[self.pos] if self.pos < len(self.text) else None
+        self.current_char = self.text[self.pos]
 
     def advance(self):
+        """Advance the `pos` and set the `current_char`."""
         self.pos += 1
-        if self.pos < len(self.text):
-            self.current_char = self.text[self.pos]
+        if self.pos > len(self.text) - 1:
+            self.current_char = None  # Indicates end of input
         else:
-            self.current_char = None
+            self.current_char = self.text[self.pos]
 
     def skip_whitespace(self):
+        """Skip whitespace characters."""
         while self.current_char is not None and self.current_char.isspace():
             self.advance()
 
-    def get_number(self):
-        result = ''
-        while self.current_char is not None and (self.current_char.isdigit() or self.current_char == '.'):
-            result += self.current_char
+    def get_mul_op(self):
+        """Get a multiplication operator token."""
+        if self.current_char == '*':
             self.advance()
-        return float(result) if '.' in result else int(result)
+            return '*'  # Token for multiplication
+        return None
 
-    def get_token(self):
-        while self.current_char is not None:
-            if self.current_char.isspace():
-                self.skip_whitespace()
-                continue
+    def get_power_op(self):
+        """Get a power operator token."""
+        if self.current_char == '^':
+            self.advance()
+            return '^'  # Token for power
+        return None
 
-            if self.current_char.isdigit() or self.current_char == '.':
-                return Token(TokenType.NUMBER, self.get_number())
+    def get_fac_op(self):
+        """Get a factorial operator token."""
+        if self.current_char == '!':
+            self.advance()
+            return '!'  # Token for factorial
+        return None
 
-            if self.current_char == '+':
-                self.advance()
-                return Token(TokenType.ADD_OP, '+')
+    def get_left_paren(self):
+        """Get a left parenthesis token."""
+        if self.current_char == '(':
+            self.advance()
+            return '('  # Token for left parenthesis
+        return None
 
-            if self.current_char == '-':
-                self.advance()
-                return Token(TokenType.SUB_OP, '-')
+    def get_right_paren(self):
+        """Get a right parenthesis token."""
+        if self.current_char == ')':
+            self.advance()
+            return ')'  # Token for right parenthesis
+        return None
 
-            if self.current_char == '*':
-                self.advance()
-                return Token(TokenType.MUL_OP, '*')
-
-            if self.current_char == '/':
-                self.advance()
-                return Token(TokenType.DIV_OP, '/')
-
-            if self.current_char == '^':
-                self.advance()
-                return Token(TokenType.POWER_OP, '^')
-
-            if self.current_char == '!':
-                self.advance()
-                return Token(TokenType.FAC_OP, '!')
-
-            if self.current_char == '(':
-                self.advance()
-                return Token(TokenType.LEFT_PAREN, '(')
-
-            if self.current_char == ')':
-                self.advance()
-                return Token(TokenType.RIGHT_PAREN, ')')
-
-        return Token(TokenType.EOF)
-
-def get_token_list(text):
-    lexer = Lexer(text)
-    tokens = []
-    while True:
-        token = lexer.get_token()
-        if token.type == TokenType.EOF:
-            break
-        tokens.append(token)
-    return tokens
-
+    # Include any other methods needed for the lexer
